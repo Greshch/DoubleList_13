12a4898f582986266eb244a709d7433b35868722
Greshch <greshchook@gmail.com>
2022-07-18 13:26:23 +0300

    ClearList() was implemented

--- DoubleList_13.cpp
+++ DoubleList_13.cpp
@@ -4,15 +4,15 @@
 #include <iostream>
 
 struct Node {
-    Node* next;
-    Node* prev;
+    Node* next = nullptr;
+    Node* prev = nullptr;
     int data;
 };
 
 struct List {
-    Node* head;
-    Node* tail;
-    int size;
+    Node* head = nullptr;
+    Node* tail = nullptr;
+    int size = 0;
 } list;
 
 void AddNode(int data);
@@ -21,9 +21,38 @@ void ShowList();
 Node* FindNode(int data);
 void AddNodeAfter(Node* node, int data);
 
+void ClearList(); // free heap memory in list
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int sizeList = 5;
+    for (int i = 0; i < sizeList; i++) // Fill list
+    {
+        AddNode(i);
+    }
+
+    ShowList();
+
+    ClearList(); // Free memory's list
+}
+
+void AddNode(int data)
+{
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->prev = list.tail;
+    ++list.size;
+
+    if (list.size == 1) // case when list is empty!
+    {
+        list.head = newNode;
+        list.tail = list.head;
+    }
+    else
+    {
+        list.tail->next = newNode;
+        list.tail = newNode;
+    }
 }
 
 void ShowList()
@@ -34,4 +63,22 @@ void ShowList()
     {
         cout << i << "#\t" << cur->data << endl;
     }
+}
+
+void ClearList()
+{
+    if (list.size == 0) // It was needed to use this one only in case where lis.size not empty
+    {
+        return;
+    }
+    Node* prev = nullptr;
+    for (Node* cur = list.head->next; cur != nullptr; cur = cur->next)
+    {
+        prev = cur->prev;
+        delete prev;
+        prev = nullptr;
+    }
+    delete list.tail; // delete tail
+    list.tail = nullptr;
+    list.size = 0;
 }
